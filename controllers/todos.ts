4cba1ff9c7f6613fd30f73fe7744db92c29fd3e8
Zaid <fpszaid@gmail.com>
2022-11-13 16:44:33 +0100

    f

--- controllers/todos.ts
+++ controllers/todos.ts
@@ -1,6 +1,13 @@
 //@ts-ignore
+const Todos = require("../models/todoModel")
+
+//@ts-ignore
 async function getAllTodos(req, res) {
-  res.status(200).send("Hello!")
+  try {
+    const results = await Todos.find()
+
+    results ? res.status(200).send(results) : res.status(500).send("Error!")
+  } catch (error) {}
 }
 
 module.exports = { getAllTodos }
