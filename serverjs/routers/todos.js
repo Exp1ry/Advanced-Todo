7e0d72b93e98f197fbb2ed55ef3ba4bd2f91ba1d
Zaid <fpszaid@gmail.com>
2022-11-13 20:58:54 +0100

    f

---
+++ serverjs/routers/todos.js
@@ -0,0 +1,9 @@
+"use strict";
+//@ts-ignore
+const { getAllTodos } = require("../controllers/todos.js");
+//@ts-ignore
+const express = require("express");
+//@ts-ignore
+const todosRouter = express.Router();
+todosRouter.route("/").get(getAllTodos);
+module.exports = todosRouter;
